48318261723d8b7b5dda3fe0668fbb250bfbc642
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-03-08 11:22:20 +0330

    [:tada:] add Row component

---
+++ packages/sonnat-ui/src/Row/Row.d.ts
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+type BaseProps<P = {}> = P & {
+  /** The content of the component. */
+  children?: React.ReactNode;
+  /**
+   * Append to the classNames applied to the component so you can override or
+   * extend the styles.
+   */
+  className?: string;
+};
+
+export type RowProps<P = {}> = BaseProps<P> &
+  Omit<React.ComponentPropsWithRef<"div">, keyof BaseProps<P>>;
+
+export interface RowFC<P = {}> {
+  // eslint-disable-next-line no-unused-vars
+  (props: RowProps<P>): JSX.Element;
+}
+
+declare const Row: RowFC<{}>;
+
+export default Row;
---
+++ packages/sonnat-ui/src/Row/Row.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import createClass from "classnames";
+import makeStyles from "../styles/makeStyles";
+
+const componentName = "Row";
+
+const useStyles = makeStyles(
+  theme => {
+    const {
+      direction,
+      spacings: { gutter },
+      typography: { pxToRem, fontFamily }
+    } = theme;
+
+    return {
+      root: {
+        direction,
+        fontFamily: fontFamily[direction],
+        display: "flex",
+        flexWrap: "wrap",
+        marginRight: pxToRem(-gutter / 2),
+        marginLeft: pxToRem(-gutter / 2)
+      }
+    };
+  },
+  { name: `Sonnat${componentName}` }
+);
+
+const Row = React.memo(
+  React.forwardRef(function Row(props, ref) {
+    const { children, className, ...otherProps } = props;
+
+    const localClass = useStyles();
+
+    return (
+      <div
+        ref={ref}
+        className={createClass(localClass.root, className)}
+        {...otherProps}
+      >
+        {children}
+      </div>
+    );
+  })
+);
+
+Row.displayName = componentName;
+
+Row.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node
+};
+
+export default Row;
