1c8cecf967fc00849d9f0c4265aca3ef6016b52f
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-03-08 11:23:27 +0330

    [:tada:] add TextArea component

---
+++ packages/sonnat-ui/src/TextArea/TextArea.d.ts
@@ -0,0 +1,108 @@
+import * as React from "react";
+
+type BaseProps<P = {}> = P & {
+  /**
+   * Append to the classNames applied to the component so you can override or
+   * extend the styles.
+   */
+  className?: string;
+  /** The name of the TextArea. */
+  name?: string;
+  /** The `placeholder` property of the TextArea. */
+  placeholder?: string;
+  /**
+   * The value of the TextArea. The DOM API casts this to a string.
+   */
+  value?: string;
+  /**
+   * The default value. Use when the component is not controlled.
+   */
+  defaultValue?: string;
+  /** The helper text content. */
+  helperText?: string;
+  /** The helper icon. */
+  helperIcon?: React.ReactNode;
+  /**
+   * If `true`, the TextArea will be vertically resizable.
+   * @default false
+   */
+  resizable?: boolean;
+  /**
+   * If `true`, the TextArea will be readOnly.
+   * @default false
+   */
+  readOnly?: boolean;
+  /**
+   * If `true`, the TextArea will be focused.
+   * @default false
+   */
+  focused?: boolean;
+  /**
+   * If `true`, the TextArea will be focused on mount.
+   * @default false
+   */
+  autoFocus?: boolean;
+  /**
+   * If `true`, the TextArea will fill the entire width of the parent.
+   * @default false
+   */
+  fluid?: boolean;
+  /**
+   * If `true`, the TextArea will be disabled.
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * If `true`, the TextArea will be required.
+   * @default false
+   */
+  required?: boolean;
+  /**
+   * If `true`, the TextArea will indicate invalid input.
+   * @default false
+   */
+  hasError?: boolean;
+  /** The properties applied to the `input` element. */
+  inputProps?: {
+    ref: React.Ref<HTMLTextAreaElement>;
+  } & React.HTMLAttributes<HTMLTextAreaElement>;
+  /**
+   * The Callback fires when the state has changed.
+   *
+   * @param {React.ChangeEvent<HTMLTextAreaElement>} event The event source of the callback.
+   * @param {string} value The current value of the TextArea.
+   *
+   * You can also pull out the current value by accessing `event.target.value` (string).
+   */
+  onChange?: (
+    /* eslint-disable no-unused-vars */
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+    value: string
+    /* eslint-enable no-unused-vars */
+  ) => void;
+  /**
+   * The Callback fires when the TextArea has received focus.
+   *
+   * @param {React.FocusEvent<HTMLTextAreaElement>} event The event source of the callback.
+   */
+  // eslint-disable-next-line no-unused-vars
+  onFocus?: (event: React.FocusEvent<HTMLTextAreaElement>) => void;
+  /**
+   * The Callback fires when the TextArea has lost focus.
+   *
+   * @param {React.FocusEvent<HTMLTextAreaElement>} event The event source of the callback.
+   */
+  // eslint-disable-next-line no-unused-vars
+  onBlur?: (event: React.FocusEvent<HTMLTextAreaElement>) => void;
+};
+export type TextAreaProps<P = {}> = BaseProps<P> &
+  Omit<React.ComponentPropsWithRef<"div">, keyof BaseProps<P>>;
+
+export interface TextAreaFC<P = {}> {
+  // eslint-disable-next-line no-unused-vars
+  (props: TextAreaProps<P>): JSX.Element;
+}
+
+declare const TextArea: TextAreaFC<{}>;
+
+export default TextArea;
---
+++ packages/sonnat-ui/src/TextArea/TextArea.js
@@ -0,0 +1,334 @@
+import React, { useState, useEffect, useRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
+import createClass from "classnames";
+import makeStyles from "../styles/makeStyles";
+import { changeColor } from "../styles/colorUtils";
+import { useFormControl } from "../FormControl";
+import { setRef, useEnhancedEffect, useControlled } from "../utils";
+
+const componentName = "TextArea";
+
+const useStyles = makeStyles(
+  theme => {
+    const {
+      colors,
+      darkMode,
+      direction,
+      typography: { pxToRem, useText, fontFamily }
+    } = theme;
+
+    return {
+      root: {
+        direction,
+        fontFamily: fontFamily[direction],
+        display: "inline-flex",
+        position: "relative",
+        minWidth: "0",
+        verticalAlign: "top",
+        flexDirection: "column",
+        "&:not($errored):not($focused) $input:hover": {
+          borderColor: !darkMode
+            ? colors.createBlackColor({ alpha: 0.48 })
+            : colors.createWhiteColor({ alpha: 0.48 })
+        }
+      },
+      input: {
+        ...useText({ color: colors.text.primary }),
+        flex: [[1, 1]],
+        minWidth: 0,
+        minHeight: pxToRem(120),
+        borderRadius: pxToRem(4),
+        outline: "none",
+        border: `${pxToRem(1)} solid ${
+          !darkMode
+            ? colors.createBlackColor({ alpha: 0.24 })
+            : colors.createWhiteColor({ alpha: 0.24 })
+        }`,
+        backgroundColor: colors.transparent,
+        resize: "none",
+        padding: [[pxToRem(8), pxToRem(16)]],
+        boxShadow: `0 0 0 0 ${colors.transparent}`,
+        WebkitAppearance: "none",
+        MozAppearance: "none",
+        appearance: "none",
+        "&::-webkit-input-placeholder": useText({
+          color: colors.text.hint
+        }),
+        "&::-moz-placeholder": useText({ color: colors.text.hint }),
+        "&:-ms-input-placeholder": useText({ color: colors.text.hint }),
+        "&:-moz-placeholder": useText({ color: colors.text.hint })
+      },
+      fluid: { width: "100%" },
+      errored: {
+        "& $input": {
+          borderColor: !darkMode ? colors.error.origin : colors.error.light
+        }
+      },
+      readOnly: {
+        pointerEvents: "none",
+        "& $input": {
+          backgroundColor: !darkMode
+            ? colors.createBlackColor({ alpha: 0.04 })
+            : colors.createWhiteColor({ alpha: 0.04 })
+        }
+      },
+      disabled: {
+        pointerEvents: "none",
+        "& $input": {
+          borderColor: colors.divider,
+          color: colors.text.disabled,
+          "&::-webkit-input-placeholder": { color: colors.text.disabled },
+          "&::-moz-placeholder": { color: colors.text.disabled },
+          "&:-ms-input-placeholder": { color: colors.text.disabled },
+          "&:-moz-placeholder": { color: colors.text.disabled }
+        }
+      },
+      focused: {
+        "&:not($errored) $input": {
+          borderColor: !darkMode
+            ? colors.createPrimaryColor({ alpha: 0.56 })
+            : changeColor(colors.primary.light, { alpha: 0.56 }),
+          boxShadow: `0 0 0 ${pxToRem(1)} ${
+            !darkMode
+              ? colors.createPrimaryColor({ alpha: 0.56 })
+              : changeColor(colors.primary.light, { alpha: 0.56 })
+          }`
+        },
+        "&$errored $input": {
+          boxShadow: `0 0 0 ${pxToRem(1)} ${
+            !darkMode ? colors.error.origin : colors.error.light
+          }`
+        }
+      },
+      resizable: { "& $input": { resize: "vertical" } }
+    };
+  },
+  { name: `Sonnat${componentName}` }
+);
+
+const TextArea = React.memo(
+  React.forwardRef(function TextArea(props, ref) {
+    const {
+      placeholder,
+      onBlur,
+      onFocus,
+      onChange,
+      className,
+      inputProps = {},
+      defaultValue,
+      name: nameProp,
+      value: valueProp,
+      autoFocus = false,
+      focused = false,
+      readOnly = false,
+      fluid = false,
+      hasError = false,
+      disabled = false,
+      resizable = false,
+      required = false,
+      ...otherProps
+    } = props;
+
+    const {
+      className: inputClassNameProp,
+      id: inputIdProp,
+      ref: inputRefProp,
+      name: inputNameProp,
+      value: inputValueProp,
+      onFocus: inputOnFocusProp,
+      onBlur: inputOnBlurProp,
+      onChange: inputOnChangeProp,
+      readOnly: inputReadOnlyProp = false,
+      autoFocus: inputAutoFocusProp = false,
+      ...otherInputProps
+    } = inputProps;
+
+    if (inputNameProp != null && nameProp != null) {
+      // eslint-disable-next-line no-console
+      console.error(
+        [
+          "Sonnat: You are passing the `name` prop twice." +
+            "(one as `name` property and the other one as a property of `inputProps`)",
+          `We are assuming \`name="${inputNameProp}"\`!`
+        ].join("\n")
+      );
+    }
+
+    if (inputValueProp != null && valueProp != null) {
+      // eslint-disable-next-line no-console
+      console.error(
+        [
+          "Sonnat: You are passing the `value` prop twice." +
+            "(one as `value` property and the other one as a property of `inputProps`)",
+          `We are assuming \`value="${inputValueProp}"\`!`
+        ].join("\n")
+      );
+    }
+
+    const name = inputNameProp || nameProp;
+
+    const inputRef = useRef();
+
+    const localClass = useStyles();
+    const formControl = useFormControl();
+
+    const [value, setValue] = useControlled(
+      inputValueProp || valueProp,
+      defaultValue,
+      componentName
+    );
+
+    const isInit = useRef(true);
+
+    const isReadOnly = !!inputReadOnlyProp || readOnly;
+    const isAutoFocus = !!inputAutoFocusProp || autoFocus || focused;
+
+    const [isMounted, setMounted] = useState(false);
+    const [isFocused, setFocused] = useState(isAutoFocus);
+
+    // inherit properties from FormControl
+    const controlProps = {
+      focused: formControl ? formControl.focusedState : isFocused,
+      disabled: formControl ? formControl.disabled : disabled,
+      hasError: formControl ? formControl.hasError : hasError,
+      required: formControl ? formControl.required : required,
+      fluid: formControl ? formControl.fluid : fluid,
+      onFocus: e => {
+        if (isMounted) {
+          e.persist();
+          if (!(controlProps.disabled || isReadOnly)) {
+            if (onFocus) onFocus(e);
+            if (inputOnFocusProp) inputOnFocusProp(e);
+            if (formControl && formControl.onFocus) formControl.onFocus(e);
+            else setFocused(true);
+          }
+        }
+      },
+      onBlur: e => {
+        if (isMounted) {
+          e.persist();
+          if (!(controlProps.disabled || isReadOnly)) {
+            if (onBlur) onBlur(e);
+            if (inputOnBlurProp) inputOnBlurProp(e);
+            if (formControl && formControl.onBlur) formControl.onBlur(e);
+            else setFocused(false);
+          }
+        }
+      },
+      onChange: e => {
+        if (isMounted) {
+          e.persist();
+          if (!(controlProps.disabled || isReadOnly)) {
+            if (onChange) onChange(e, e.target.value);
+            if (inputOnChangeProp) inputClassNameProp(e, e.target.value);
+            setValue(e.target.value);
+          }
+        }
+      }
+    };
+
+    // prevent component from being focused if it is disabled or readOnly
+    controlProps.focused =
+      controlProps.disabled || isReadOnly ? false : controlProps.focused;
+
+    useEffect(() => {
+      setMounted(true);
+      return () => setMounted(false);
+    }, []);
+
+    // initially focus the component if it is focused
+    useEnhancedEffect(() => {
+      if (
+        isInit.current &&
+        isMounted &&
+        !(controlProps.disabled || isReadOnly)
+      ) {
+        if (isAutoFocus || controlProps.focused) {
+          if (inputRef.current) {
+            inputRef.current.focus();
+            isInit.current = false;
+          }
+        }
+      }
+    });
+
+    useImperativeHandle(ref, () => ({
+      focus: () => {
+        inputRef.current.focus();
+      },
+      blur: () => {
+        inputRef.current.blur();
+      },
+      clear: () => {
+        if (value !== "") {
+          if (!(controlProps.disabled || isReadOnly)) {
+            if (onChange) onChange(undefined, "");
+            if (inputOnChangeProp) inputOnChangeProp(undefined, "");
+            inputRef.current.value = "";
+            setValue("");
+          }
+        }
+      }
+    }));
+
+    return (
+      <div
+        className={createClass(localClass.root, className, {
+          [localClass.resizable]: resizable,
+          [localClass.focused]: controlProps.focused,
+          [localClass.disabled]: controlProps.disabled,
+          [localClass.readOnly]: isReadOnly,
+          [localClass.fluid]: controlProps.fluid,
+          [localClass.errored]: controlProps.hasError
+        })}
+        ref={ref}
+        {...otherProps}
+      >
+        <textarea
+          id={inputIdProp}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          disabled={controlProps.disabled}
+          required={controlProps.required}
+          className={createClass(localClass.input, inputClassNameProp)}
+          onChange={controlProps.onChange}
+          onFocus={controlProps.onFocus}
+          onBlur={controlProps.onBlur}
+          readOnly={isReadOnly}
+          ref={node => {
+            if (inputRefProp) setRef(inputRefProp, node);
+            setRef(inputRef, node);
+          }}
+          {...otherInputProps}
+        ></textarea>
+      </div>
+    );
+  })
+);
+
+TextArea.displayName = componentName;
+
+TextArea.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  defaultValue: PropTypes.string,
+  helperText: PropTypes.string,
+  helperIcon: PropTypes.node,
+  readOnly: PropTypes.bool,
+  focused: PropTypes.bool,
+  autoFocus: PropTypes.bool,
+  disabled: PropTypes.bool,
+  resizable: PropTypes.bool,
+  required: PropTypes.bool,
+  hasError: PropTypes.bool,
+  fluid: PropTypes.bool,
+  onFocus: PropTypes.func,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  inputProps: PropTypes.object
+};
+
+export default TextArea;
