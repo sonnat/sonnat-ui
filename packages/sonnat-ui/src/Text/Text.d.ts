6b7dbcdfaf97c8d4fa6c593b8b2a2cfb6130bb36
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-03-08 11:23:18 +0330

    [:tada:] add Text component

---
+++ packages/sonnat-ui/src/Text/Text.d.ts
@@ -0,0 +1,104 @@
+import * as React from "react";
+import {
+  OverridableComponent,
+  OverrideProps
+} from "../utils/typings/OverridableComponent";
+
+export type TextTypeMap<P = {}, N extends React.ElementType = "span"> = {
+  defaultRootNode: N;
+  props: P & {
+    /**
+     * Applies the theme typography styles.
+     */
+    variant:
+      | "h1"
+      | "h2"
+      | "h3"
+      | "h4"
+      | "h5"
+      | "h6"
+      | "heroText"
+      | "titleText"
+      | "bodyText"
+      | "captionText";
+    /**
+     * The content of the component.
+     */
+    children?: React.ReactNode;
+    /**
+     * Append to the classNames applied to the component so you can override or
+     * extend the styles.
+     */
+    className?: string;
+    /**
+     * The color of the text.
+     * @default "inherit"
+     */
+    color?:
+      | "inherit"
+      | "textPrimary"
+      | "textSecondary"
+      | "textHint"
+      | "textDisabled"
+      | "primary"
+      | "secondary"
+      | "error"
+      | "success"
+      | "warning"
+      | "info" = "inherit";
+    /**
+     * Set the text-align on the text.
+     */
+    align?: "initial" | "inherit" | "left" | "center" | "right" | "justify";
+    /**
+     * Set the display on the component.
+     * @default "initial"
+     */
+    display?:
+      | "initial"
+      | "block"
+      | "inline"
+      | "inline-block"
+      | "flex"
+      | "inline-flex";
+    /**
+     * Set the text-overflow on the text.
+     * @default "ellipsis"
+     */
+    textOverflow?: "clip" | "ellipsis";
+    /**
+     * Applies the weighted typography styles based on the theme.
+     */
+    weight?: "bolder" | "lighter";
+    /**
+     * Applies the sized typography styles based on the theme.
+     * @default "large"
+     */
+    size?: "large" | "medium" | "small" | "extraSmall";
+    /**
+     * If `true`,
+     * the responsive behaviour of this typography class will apply to the component.
+     * @default false
+     */
+    responsive?: boolean;
+    /**
+     * If `true`, the text will not wrap,
+     * but instead will truncate or clip based on the `textOverflow` prop provided.
+     *
+     * Note that text overflow can only happen
+     * when the element has a width in order to overflow.
+     * ('block', 'inline-block', 'flex', 'inline-flex')
+     * @default false
+     */
+    noWrap?: boolean;
+  };
+};
+
+export type TextProps<
+  P = {},
+  N extends React.ElementType = TextTypeMap["defaultRootNode"]
+> = OverrideProps<TextTypeMap<P, N>, N>;
+
+declare const Text: OverridableComponent<TextTypeMap<{}, "span">>;
+
+export default Text;
---
+++ packages/sonnat-ui/src/Text/Text.js
@@ -0,0 +1,271 @@
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import createClass from "classnames";
+import makeStyles from "../styles/makeStyles";
+
+const componentName = "Text";
+
+const variantEnum = [
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "heroText",
+  "titleText",
+  "bodyText",
+  "captionText"
+];
+
+const colorEnum = [
+  "inherit",
+  "textPrimary",
+  "textSecondary",
+  "textHint",
+  "textDisabled",
+  "primary",
+  "secondary",
+  "error",
+  "success",
+  "warning",
+  "info"
+];
+
+const alignEnum = ["left", "right", "center", "justify", "inherit", "initial"];
+const displayEnum = [
+  "inline",
+  "block",
+  "inline-block",
+  "flex",
+  "inline-flex",
+  "inherit"
+];
+const textOverflowEnum = ["clip", "ellipsis"];
+const sizeEnum = ["large", "medium", "small", "extraSmall"];
+const weightEnum = ["bolder", "lighter"];
+
+const camelCase = s => s.replace(/-./g, x => x.toUpperCase()[1]);
+const getCorrectVariantKey = (variant, size, weight, sourceOfTruth) => {
+  const validation = { variant: true, size: true, weight: true };
+
+  if (!variant || !variantEnum.includes(variant)) validation.variant = false;
+  if (!size || !sizeEnum.includes(size)) validation.size = false;
+  if (!weight || !weightEnum.includes(weight)) validation.weight = false;
+
+  if (!validation.variant) return "";
+  else {
+    let key = `${variant}`;
+    const variantKey = key;
+    const hasVariant = sourceOfTruth[key];
+
+    if (!validation.size) return hasVariant ? variantKey : "";
+    else {
+      key += `-${size}`;
+      const variantKeyWithSize = camelCase(key);
+      const hasVariantWithSize = sourceOfTruth[variantKeyWithSize];
+
+      if (!validation.weight)
+        return hasVariantWithSize
+          ? variantKeyWithSize
+          : hasVariant
+          ? variantKey
+          : "";
+      else {
+        key += `-${weight}`;
+        const variantKeyWithSizeWithWeight = camelCase(key);
+        const hasVariantWithSizeWithWeight =
+          sourceOfTruth[variantKeyWithSizeWithWeight];
+
+        return hasVariantWithSizeWithWeight
+          ? variantKeyWithSizeWithWeight
+          : hasVariantWithSize
+          ? variantKeyWithSize
+          : hasVariant
+          ? variantKey
+          : "";
+      }
+    }
+  }
+};
+
+const generateVariations = (variants, breakpoints) => {
+  const styles = {};
+  const responsiveStyles = {};
+
+  alignEnum.forEach(alignment => {
+    styles[`${alignment}Alignment`] = { textAlign: alignment };
+  });
+
+  displayEnum.forEach(display => {
+    styles[`${camelCase(display)}Display`] = { display };
+  });
+
+  textOverflowEnum.forEach(overflow => {
+    styles[`${overflow}Overflow`] = { textOverflow: overflow };
+  });
+
+  variantEnum.forEach(v => {
+    const variant = variants[v];
+
+    if (!variant) return;
+
+    if (variant.rules) styles[v] = variant.rules;
+    if (variant.responsiveRules)
+      responsiveStyles[`${v}`] = {
+        "&$responsive": variant.responsiveRules
+      };
+
+    sizeEnum.forEach(size => {
+      if (variant[size]) {
+        if (variant[size].rules) {
+          const key = camelCase(`${v}-${size}`);
+          styles[key] = variant[size].rules;
+        }
+
+        if (variant[size].responsiveRules) {
+          const key = camelCase(`${v}-${size}`);
+          responsiveStyles[`${key}`] = {
+            "&$responsive": variant[size].responsiveRules
+          };
+        }
+
+        weightEnum.forEach(weight => {
+          if (variant[size][weight]) {
+            if (variant[size][weight].rules) {
+              const key = camelCase(`${v}-${size}-${weight}`);
+              styles[`${key}`] = variant[size][weight].rules;
+            }
+
+            if (variant[size][weight].responsiveRules) {
+              const key = camelCase(`${v}-${size}-${weight}`);
+              responsiveStyles[`${key}`] = {
+                "&$responsive": variant[size][weight].responsiveRules
+              };
+            }
+          }
+        });
+      }
+    });
+  });
+
+  return { ...styles, [breakpoints.down("sm")]: responsiveStyles };
+};
+
+const useStyles = makeStyles(
+  theme => {
+    const {
+      colors,
+      darkMode,
+      breakpoints,
+      direction,
+      typography: { variants, fontFamily }
+    } = theme;
+
+    return {
+      root: {
+        direction,
+        fontFamily: fontFamily[direction],
+        textDecoration: "none"
+      },
+      noWrap: {
+        whiteSpace: "nowrap",
+        overflow: "hidden"
+      },
+      inheritColor: { color: "inherit" },
+      textPrimaryColor: { color: colors.text.primary },
+      textSecondaryColor: { color: colors.text.secondary },
+      textHintColor: { color: colors.text.hint },
+      textDisabledColor: { color: colors.text.disabled },
+      primaryColor: {
+        color: !darkMode ? colors.primary.origin : colors.primary.light
+      },
+      secondaryColor: {
+        color: !darkMode ? colors.secondary.origin : colors.secondary.light
+      },
+      errorColor: {
+        color: !darkMode ? colors.error.origin : colors.error.light
+      },
+      successColor: {
+        color: !darkMode ? colors.success.origin : colors.success.light
+      },
+      warningColor: {
+        color: !darkMode ? colors.warning.origin : colors.warning.light
+      },
+      infoColor: { color: !darkMode ? colors.info.origin : colors.info.light },
+      responsive: {},
+      ...generateVariations(variants, breakpoints)
+    };
+  },
+  { name: `Sonnat${componentName}` }
+);
+
+const Text = React.memo(
+  React.forwardRef(function Text(props, ref) {
+    const {
+      children,
+      className,
+      variant,
+      align,
+      weight,
+      display,
+      rootNode: HTMLTag = "span",
+      textOverflow = "ellipsis",
+      color = "inherit",
+      size = "large",
+      noWrap = false,
+      responsive = false,
+      ...otherProps
+    } = props;
+
+    const localClass = useStyles();
+
+    const variantKey = useMemo(
+      () => getCorrectVariantKey(variant, size, weight, localClass),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [variant, size, weight]
+    );
+
+    return (
+      <HTMLTag
+        ref={ref}
+        className={createClass(
+          className,
+          localClass.root,
+          localClass[variantKey],
+          localClass[`${display}Display`],
+          localClass[`${color}Color`],
+          localClass[`${textOverflow}Overflow`],
+          {
+            [localClass.noWrap]: noWrap,
+            [localClass.responsive]: responsive,
+            [localClass[`${align}Alignment`]]: align != null && !!align,
+            [localClass[`${display}Display`]]: display != null && !!display
+          }
+        )}
+        {...otherProps}
+      >
+        {children}
+      </HTMLTag>
+    );
+  })
+);
+
+Text.displayName = componentName;
+
+Text.propTypes = {
+  children: PropTypes.node,
+  variant: PropTypes.oneOf(variantEnum).isRequired,
+  size: PropTypes.oneOf(sizeEnum),
+  rootNode: PropTypes.elementType,
+  align: PropTypes.oneOf(alignEnum),
+  color: PropTypes.oneOf(colorEnum),
+  display: PropTypes.oneOf(displayEnum),
+  textOverflow: PropTypes.oneOf(textOverflowEnum),
+  weight: PropTypes.oneOf(weightEnum),
+  className: PropTypes.string,
+  responsive: PropTypes.bool,
+  noWrap: PropTypes.bool
+};
+
+export default Text;
