53f33685bdc3ca18ff36e862ff8eeba5b1f0760b
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-04-03 11:20:50 +0430

    [:bug:] fixed an issue that prevented theme to be updated

--- packages/sonnat-ui/src/styles/useDarkMode/useDarkMode.js
+++ packages/sonnat-ui/src/styles/useDarkMode/useDarkMode.js
@@ -1,19 +1,26 @@
+import React from "react";
 import { dark, light } from "../createColors";
 import usePreviousValue from "../../utils/usePreviousValue";
 import defaultTheme from "../defaultTheme";
 
 export default function useDarkMode(isDarkMode = false, theme = defaultTheme) {
-  let currentTheme = theme;
-
   const prevState = usePreviousValue(isDarkMode);
+  const cachedTheme = React.useRef(theme);
 
-  if (prevState != null && isDarkMode !== prevState) {
-    return {
-      ...theme,
-      colors: { ...theme.colors, ...(isDarkMode ? dark : light) },
-      darkMode: isDarkMode
-    };
-  }
+  const newTheme = React.useMemo(() => {
+    if (prevState != null && isDarkMode !== prevState) {
+      return {
+        ...cachedTheme.current,
+        colors: {
+          ...cachedTheme.current.colors,
+          ...(isDarkMode ? dark : light)
+        },
+        darkMode: isDarkMode
+      };
+    } else return cachedTheme.current;
+  }, [prevState, isDarkMode]);
 
-  return currentTheme;
+  cachedTheme.current = newTheme;
+
+  return newTheme;
 }
