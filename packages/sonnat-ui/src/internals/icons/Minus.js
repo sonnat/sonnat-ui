8634f7ed93111ee0c5aa4e01a71f1af61a81f4c5
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-05-11 13:38:53 +0430

    [:star:] add internal svg icons

---
+++ packages/sonnat-ui/src/internals/icons/Minus.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import createSvgIcon from "../../utils/createSvgIcon";
+
+const Minus = createSvgIcon(
+  <path d="M17 13H7C6.447 13 6 12.552 6 12C6 11.448 6.447 11 7 11H17C17.553 11 18 11.448 18 12C18 12.552 17.553 13 17 13Z" />,
+  "Minus"
+);
+
+export default Minus;
