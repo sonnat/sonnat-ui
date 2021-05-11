fa66acfb286bf3f9b771e3917eff3168ade596e2
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-05-11 13:40:50 +0430

    [:sparkles:] add createSvgIcon utility function

---
+++ packages/sonnat-ui/src/utils/createSvgIcon.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import SonnatIcon from "../Icon";
+
+export default function createSvgIcon(children, name) {
+  const Icon = React.memo(
+    React.forwardRef((props, ref) => (
+      <SonnatIcon viewBox="0 0 24 24" ref={ref} {...props}>
+        {children}
+      </SonnatIcon>
+    ))
+  );
+
+  Icon.displayName = `${name}Icon`;
+
+  return Icon;
+}
