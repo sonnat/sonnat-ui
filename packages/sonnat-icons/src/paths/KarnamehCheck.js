6fcdca9b53992d3e4355998419e7478825b49496
Mostafa Shamsitabar <mostafa.shamsitabar@cafebazaar.ir>
2021-05-12 18:52:58 +0430

    [:recycle:] rebuild icons with `fillRule` and `clipRule` attributes

--- packages/sonnat-icons/src/paths/KarnamehCheck.js
+++ packages/sonnat-icons/src/paths/KarnamehCheck.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export default (
-  <path d="M10.082 8.99051L6.076 5.70751V9.22151L10.203 12.6025L18.612 5.71051V1.99951L10.082 8.99051Z" />
+  <path fillRule="evenodd" clipRule="evenodd" d="M10.082 8.99051L6.076 5.70751V9.22151L10.203 12.6025L18.612 5.71051V1.99951L10.082 8.99051Z" />
 );
--- packages/sonnat-icons/src/paths/Minus.js
+++ packages/sonnat-icons/src/paths/Minus.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export default (
-  <path d="M17 13H7C6.447 13 6 12.552 6 12C6 11.448 6.447 11 7 11H17C17.553 11 18 11.448 18 12C18 12.552 17.553 13 17 13Z" />
+  <path fillRule="evenodd" clipRule="evenodd" d="M17 13H7C6.447 13 6 12.552 6 12C6 11.448 6.447 11 7 11H17C17.553 11 18 11.448 18 12C18 12.552 17.553 13 17 13Z" />
 );
