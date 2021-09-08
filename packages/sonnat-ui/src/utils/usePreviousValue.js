f666238aa99ba7ffd1a84a76f5f47d3b5b89e5f9
mimshins <mostafa.sh.coderino@gmail.com>
2021-09-08 17:31:58 +0430

    [:recycle:] remove the comments

--- packages/sonnat-ui/src/utils/usePreviousValue.js
+++ packages/sonnat-ui/src/utils/usePreviousValue.js
@@ -1,15 +1,11 @@
 import { useRef, useEffect } from "react";
 
 export default function usePreviousValue(value) {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
   const ref = useRef();
 
-  // Store current value in ref
   useEffect(() => {
     ref.current = value;
-  }, [value]); // Only re-run if value changes
+  }, [value]);
 
-  // Return previous value (happens before update in useEffect above)
   return ref.current;
 }
