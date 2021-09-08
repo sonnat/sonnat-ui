6167f884be76e980782612b49783b79b016c1f66
mimshins <mostafa.sh.coderino@gmail.com>
2021-09-08 17:32:12 +0430

    [TS] update the type declaration

--- packages/sonnat-ui/src/utils/usePreviousValue.d.ts
+++ packages/sonnat-ui/src/utils/usePreviousValue.d.ts
@@ -4,6 +4,4 @@
  * @param {T} value The current value to be cached.
  * @returns Returns the previous cached value.
  */
-/* eslint-disable */
-export default function usePreviousValue<T>(value: T): (value: T) => T;
-/* eslint-enable */
+export default function usePreviousValue<T>(value: T): T | undefined;
