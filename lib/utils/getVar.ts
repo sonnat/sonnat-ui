68f02fcf22d883ac31561b4e110fc9a9668d4a5b
mimshins <mostafa.sh.coderino@gmail.com>
2022-01-16 14:26:32 +0330

    [:bug:] fix a bug related to the conditions

--- lib/utils/getVar.ts
+++ lib/utils/getVar.ts
@@ -3,5 +3,6 @@ const getVar = <T>(
   fallback: NonNullable<T>,
   condition = true
 ): NonNullable<T> =>
-  variable == null && condition ? fallback : (variable as NonNullable<T>);
+  variable == null || condition ? fallback : (variable as NonNullable<T>);
+
 export default getVar;
